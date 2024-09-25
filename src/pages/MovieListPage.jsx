cc16372edc8d6cb2e1aa8fa415823a4ca8bf1150
Vitor Musolino Teixeira <160746719+vitormusolino@users.noreply.github.com>
2024-09-24 21:02:38 -0300

    Add files via upload

--- src/pages/MovieListPage.jsx
+++ src/pages/MovieListPage.jsx
@@ -1,10 +1,42 @@
+import { useState } from "react";
+import movies from "../data/movies.json"
+import MovieCard from "../components/MovieCard"
+
 export default function MovieListPage(){
+
+    const [search, setSearch] = useState('')
+    const[listaFilmes, setListaFilmes] = useState(movies)
+
+    const handleSearch= (e) => {
+        setSearch(e.target.value)
+            
+    }
+
+
     return(
         <>
-        <h1>Movie List Page</h1>
-        {/* Exibe a lista de todos os filmes disponíveis categorizados por gênero.
-        Cada filme na lista é mostrado com uma imagem, 
-        título e um link para a página de detalhes do filme.*/}
+        <h2 className="">Veja o catálogo completo dos filmes</h2>
+
+        <input 
+        className="text-black"
+        type="text" 
+        name="search"
+        id="search"
+        placeholder="O que procura caro amigo?"
+        value={search}
+        onChange={handleSearch}
+        />
+
+        <div className="flex">
+            {
+            listaFilmes
+                .filter( filme => (filme.titulo).includes(search))
+                .map(filme => (
+                <MovieCard key={filme.id}{...filme}/>
+            ))
+            }
+        </div>
         </>
     )
+
 }
