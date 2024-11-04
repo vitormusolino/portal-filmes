ed43c8675c606716647e913584cfedc6b261cb6a
Vitor Musolino Teixeira <160746719+vitormusolino@users.noreply.github.com>
2024-11-04 11:09:19 -0300

    Add files via upload

--- src/components/GenreCard.jsx
+++ src/components/GenreCard.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom"
 
-export default function GenreCard({name,id}) {
+export default function GenreCard({name,key}) {
     return(
         <>
         <div className="flex justify-center items-center  bg-purple-700 px-20 py-10 max-w-52 min-w-52 max-h-28 min-h-28 rounded-md">
-            <Link to={`/genre/${id}`}><h1 className="">{name}</h1></Link>
+            <Link to={`/genre/:${key}`}><h1 className="">{name}</h1></Link>
         </div>
         
         
--- src/pages/MoviesByGenrePage.jsx
+++ src/pages/MoviesByGenrePage.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import MovieCard from "../components/MovieCard";
 
 export default function MoviesByGenrePage(){
     
     const [genero,setGenero] = useState([])
-    
+    const {id} = useParams()
 
     useEffect( () => {
         fetch(`https://api.themoviedb.org/3/discover/movie?api_key=7c572a9f5b3ba776080330d23bb76e1e&language=pt-br&with_genres=${id}`)
@@ -17,6 +19,7 @@ export default function MoviesByGenrePage(){
     return(
         <>
         <h1>Filmes do Genêro:</h1>
+        <MovieCard />
         </>
     )
 }
